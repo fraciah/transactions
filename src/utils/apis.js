590720de4fe646978d5639854ad2649c4aeeece9
Fraciah Karagu <karfraciah@gmail.com>
2025-06-03 15:37:36 +0300

    ✨ Add apis

---
+++ src/utils/apis.js
@@ -0,0 +1,6 @@
+export const VITE_API_URL = import.meta.env.VITE_API_URL;
+
+export const LOGIN = VITE_API_URL + "/api/auth/login";
+export const LOGOUT= VITE_API_URL + "/api/auth/logout";
+export const TRANSACTIONS= VITE_API_URL + "/api/transactions";
+ 
